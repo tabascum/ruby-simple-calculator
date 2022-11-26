3f224b856c7388fa007d5792c0904c335d5b64c1
tabascum <marques_vasco@yahoo.es>
2022-11-26 23:21:00 +0000

    Upload my first Ruby project

---
+++ README.md
@@ -0,0 +1,5 @@
+To complement my learning and development of projects in web development, I decided to learn Ruby.
+
+In this project I use conditions to present a simple calculator.
+
+To make it usable as it is, it will be necessary to use terminal in VS Code and the command "ruby calculator.rb"
---
+++ calculator.rb
@@ -0,0 +1,57 @@
+result = ""
+
+loop do 
+    puts result
+    puts "Select one of the following options"
+    puts "1 - Do a math operation"
+    puts "0 - End"
+    print "Option: "
+
+    option = gets.chomp.to_i
+
+    if option == 1
+        puts "Choose your mathematical operation: "
+        puts "1 - Sum"
+        puts "2 - Substract"
+        puts "3 - Multiply"
+        puts "4 - Divide"
+
+        option = gets.chomp.to_i
+
+        if option == 1
+            puts "Digit your first number"
+            number1 = gets.chomp.to_i
+            puts "Digit your second number"
+            number2 = gets.chomp.to_i
+            number = number1 + number2
+            result = "The sum of #{number1} and #{number2} is #{number}" 
+        elsif option == 2
+            puts "Digit your first number"
+            number1 = gets.chomp.to_i
+            puts "Digit your second number"
+            number2 = gets.chomp.to_i
+            number = number1 - number2
+            result = "The sum of #{number1} and #{number2} is #{number}" 
+        elsif option == 3
+            puts "Digit your first number"
+            number1 = gets.chomp.to_i
+            puts "Digit your second number"
+            number2 = gets.chomp.to_i
+            number = number1 * number2
+            result = "The sum of #{number1} and #{number2} is #{number}" 
+        elsif option == 4
+            puts "Digit your first number"
+            number1 = gets.chomp.to_i
+            puts "Digit your second number"
+            number2 = gets.chomp.to_i
+            number = number1 - number2
+            result = "The sum of #{number1} and #{number2} is #{number}" 
+        end
+    elsif option == 0
+        break
+    else
+        result = "Select a valid option"
+    end
+
+    system "clear"
+end
