47fb1da2415aceec7ba336b4cdb1bb441f6552ed
tabascum <marques_vasco@yahoo.es>
2022-11-27 16:30:22 +0000

    Fix text message when result is displayed

--- calculator.rb
+++ calculator.rb
@@ -31,21 +31,21 @@ loop do
             puts "Digit your second number"
             number2 = gets.chomp.to_i
             number = number1 - number2
-            result = "The sum of #{number1} and #{number2} is #{number}" 
+            result = "The substraction of #{number1} from #{number2} is #{number}" 
         elsif option == 3
             puts "Digit your first number"
             number1 = gets.chomp.to_i
             puts "Digit your second number"
             number2 = gets.chomp.to_i
             number = number1 * number2
-            result = "The sum of #{number1} and #{number2} is #{number}" 
+            result = "The multiplication of #{number1} and #{number2} is #{number}" 
         elsif option == 4
             puts "Digit your first number"
             number1 = gets.chomp.to_i
             puts "Digit your second number"
             number2 = gets.chomp.to_i
             number = number1 - number2
-            result = "The sum of #{number1} and #{number2} is #{number}" 
+            result = "The division of #{number1} and #{number2} is #{number}" 
         end
     elsif option == 0
         break
